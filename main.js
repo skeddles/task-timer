5aa54321de1071a27590b68dbfedc0f01c5d4051
Sam Keddy <skeddy13@yahoo.com>
2022-04-20 14:15:47 -0400

    init

---
+++ main.js
@@ -0,0 +1,133 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+const path = require('path')
+
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+var electron = require('electron');
+
+var width = 300;
+var height = 400;
+var taskbarHeight = 40;
+var tray;
+
+
+function openTimer() {
+	timerWindow = new BrowserWindow({
+		width: 150,
+		height: 100,
+		webPreferences: {
+			nodeIntegration: true
+		},
+		icon: __dirname + '/build/icon.ico',
+		frame: false,
+		resizable: false,
+		alwaysOnTop: true,
+	});
+  
+	timerWindow.loadFile('timer.htm');
+}
+/*
+// Create the browser window.
+function createWindow() {
+
+	tray = new Tray('images/tray_icon1.png')
+	const contextMenu = Menu.buildFromTemplate([
+
+		{ label: 'Show dev tools', type:'checkbox', click: (menuItem, browserWindow, event)=>{
+			mainWindow.toggleDevTools();
+		}},
+		{ label: 'Restart', click: (menuItem, browserWindow, event)=>{
+			app.relaunch();
+			app.exit();
+		}},
+		{ label: 'Open Timer', click: (menuItem, browserWindow, event)=>{
+			openTimer();
+		}},
+		{ label: 'Exit', type: 'normal', role: 'quit' }
+	]);
+	tray.setToolTip('Taskedd');
+	tray.setContextMenu(contextMenu);
+
+	animateTrayIcon();
+
+	var screen = electron.screen.getPrimaryDisplay().size;
+
+	mainWindow = new BrowserWindow({
+		title: 'Taskedd',
+		width: width,
+		height: height,
+		minWidth: width,
+		minHeight: height,
+		maxWidth: width,
+		maxHeight: screen.height - 100,
+		movable: false,
+		alwaysOnTop: true,
+		icon: 'tray.ico',
+		frame: false,
+		skipTaskbar: true,
+		x: screen.width - width,
+		y: screen.height - height - taskbarHeight,
+		webPreferences: {
+			//preload: path.join(__dirname, 'tasks.js'),
+			nodeIntegration: true
+		}
+	});
+
+	// and load the index.html of the app.
+	mainWindow.loadFile('tasks.htm')
+
+	tray.on('click', e=>{
+		
+		
+		if (mainWindow.isVisible())
+			mainWindow.hide();
+		else 
+			mainWindow.show();
+	});
+
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+
+
+		mainWindow = null;
+
+	});
+}
+
+*/
+
+
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', openTimer)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) openTimer()
+})
+
+
+app.requestSingleInstanceLock();
+
+app.on('second-instance', (event, argv, cwd) => {
+	app.quit();
+});
---
+++ timer.js
@@ -0,0 +1,181 @@
+//window
+const electron = require("electron");
+const remote = electron.remote;
+const BrowserWindow = remote.BrowserWindow;
+
+//configration constants
+const SPEECH_VOLUME = 0.9;
+const MINUTES_BETWEEN_REMINDER = 5;
+const SHAKE_FRAME_LENGTH = 50; //how long each frame lasts in shake()
+const SHAKE_FRAME_SKIP = 3; //how many frames to skip before toggling flash
+
+//global bars
+var timerLength, countDownDate, interval;
+
+//elements
+const minutesInput = document.getElementById('minutes');
+const taskInput = document.getElementById('taskname');
+const start = document.getElementById('start');
+const timer = document.getElementById('timer');
+const setup = document.getElementById('setup');
+const timeleft = document.getElementById('timeleft');
+const currenttask = document.getElementById('currenttask');
+
+//audio
+var doneSound = new Audio('done.mp3');
+var startSound = new Audio('start.mp3');
+
+//windows
+const timerWindow = remote.getCurrentWindow();
+
+const borderWindow = new BrowserWindow({
+	width: screen.width,
+	height: screen.height,
+	x: 0,
+	y: 0,
+	webPreferences: {
+		nodeIntegration: true
+	},
+	frame: false,
+	resizable: false,
+	alwaysOnTop: true,
+	hasShadow: false,
+	parent: timerWindow,
+	transparent: true,
+	skipTaskbar: true,
+	show: false,
+});
+
+borderWindow.loadFile('border.htm');
+
+borderWindow.setIgnoreMouseEvents(true);
+
+
+function update() {
+
+	// Get todays date and time
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in the element with id="demo"
+	timeleft.innerHTML = minutes.toString().padStart(2, '00') + ":" + seconds.toString().padStart(2, '00');
+
+
+	//countdown finished
+	if (distance < 0) {
+		//reset timer
+		clearInterval(interval);
+		timeleft.innerHTML = "00:00";
+		
+		shake();
+
+		//play sound
+		doneSound.play();
+		say(currenttask.innerHTML + ' complete');
+
+		//show main screen
+		setup.style.visibility = 'visible';
+		timer.style.visibility = 'hidden';
+
+		borderWindow.hide();
+	}
+
+	
+	//at the end of every minute
+	else if (seconds == 0) {
+		shake();
+		startSound.play();
+
+		if (minutes % MINUTES_BETWEEN_REMINDER == 0) {
+
+			//say current task
+			say(currenttask.innerHTML);
+			shake();
+		}
+	}
+}
+
+
+start.addEventListener('click', function () {
+
+
+	console.log('aASDFA', minutesInput)
+
+	// Set the date we're counting down to
+	countDownDate = new Date().getTime() + minutesInput.value * 60 * 1000;
+
+	// Update the count down every 1 second
+	interval = setInterval(update, 1000);
+
+	//set start time
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	timeleft.innerHTML = minutes.toString().padStart(2, '00') + ":" + seconds.toString().padStart(2, '00');
+
+	//set task name
+	currenttask.innerHTML = taskInput.value || 'Work';
+
+	//show timer screen
+	setup.style.visibility = 'hidden';
+	timer.style.visibility = 'visible';
+
+	//show border
+	borderWindow.show();
+
+	//play sound
+	startSound.play();
+	say('Starting task ' + taskInput.value);
+});
+
+function say(text) {
+	var speech = new SpeechSynthesisUtterance(text);
+	speech.volume = SPEECH_VOLUME;
+	speechSynthesis.speak(speech);
+}
+
+
+function shake() {
+	var x = timerWindow.getPosition()[0];
+	var y = timerWindow.getPosition()[1];
+
+	var frames = [
+		{ x: -5, y: -5 },
+		{ x: -5, y: +5 },
+		{ x: +5, y: +5 },
+		{ x: +5, y: -5 },
+		{ x: -5, y: -5 },
+		{ x: -5, y: +5 },
+		{ x: +5, y: +5 },
+		{ x: +5, y: -5 },
+		{ x: -5, y: -5 },
+		{ x: -5, y: +5 },
+		{ x: +5, y: +5 },
+		{ x: +5, y: -5 },
+		{ x: -5, y: -5 },
+		{ x: -5, y: +5 },
+		{ x: +5, y: +5 },
+		{ x: +5, y: -5 },
+	]
+
+	//schedule each frame
+	frames.forEach((frame, i) => {
+		setTimeout(() => {
+			//flash frame
+			if (i % SHAKE_FRAME_SKIP == 0) timer.classList.toggle('flash');
+
+			//move window
+			timerWindow.setPosition(x + frame.x, y + frame.y);
+		}, SHAKE_FRAME_LENGTH * i);
+	});
+
+	//reset at end
+	setTimeout(() => {
+		timer.classList.remove('flash');
+		timerWindow.setPosition(x, y);
+	}, SHAKE_FRAME_LENGTH * frames.length);
+
+}
